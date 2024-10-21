eb5d46f50bed93d05dc49b5f47ce095ca295ba15
PaulK841 <118380695+PaulK841@users.noreply.github.com>
2024-10-21 11:36:11 +0200

    Update script.js

--- datacamp/src/script.js
+++ datacamp/src/script.js
@@ -158,18 +158,3 @@ function populateTopTracks(tracks) {
         topTracks.appendChild(li);
     });
 }
-
-function dotp(x, y) {
-    function dotp_sum(a, b) {
-      return a + b;
-    }
-    function dotp_times(a, i) {
-      return x[i] * y[i];
-    }
-    return x.map(dotp_times).reduce(dotp_sum, 0);
-  }
-  
-function cosineSimilarity(A,B){
-    var similarity = dotp(A, B) / (Math.sqrt(dotp(A,A)) * Math.sqrt(dotp(B,B)));
-    return similarity;
-  }
