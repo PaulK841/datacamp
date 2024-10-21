6b68a137207e8aa1cbc22ed0d64f237022fdd34a
JulesSucrot <51828364+JulesSucrot@users.noreply.github.com>
2024-10-21 14:43:51 +0200

    Update script_authentification.js

--- datacamp/SpotyHub/src/script_authentification.js
+++ datacamp/SpotyHub/src/script_authentification.js
@@ -1,5 +1,5 @@
 // Définir l'ID du client pour l'application Spotify
-const clientId = "d8d7799d31d84752873ad62f05a31226"; //Client ID de Tristan
+const clientId = "a80cc994deaf48b6a7363687970d729b"; //Client ID de Tristan
 
 // Extraire le paramètre "code" de l'URL. Il s'agit du code d'autorisation fourni par Spotify pour obtenir un access_token.
 const params = new URLSearchParams(window.location.search);
