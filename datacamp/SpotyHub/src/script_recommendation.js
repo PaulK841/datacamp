fe17a03f65ea4d5cb779f73b2254a51e92e27c1a
PaulK841 <118380695+PaulK841@users.noreply.github.com>
2024-10-21 19:45:04 +0200

    azf

--- datacamp/SpotyHub/src/script_recommendation.js
+++ datacamp/SpotyHub/src/script_recommendation.js
@@ -1,8 +1,7 @@
-import { fetchAudioFeatures } from './script_statistique.js';  // Import de la fonction asynchrone
-
+import fetchAudioFeatures from './script_statistique.js';  // Import de la fonction asynchrone
+fetchAudioFeatures=require ('./script_statistique.js')
 document.addEventListener('DOMContentLoaded', async function() {
   const url = 'https://raw.githubusercontent.com/PaulK841/datacamp/main/datacamp/SpotyHub/src/dataset.csv';
-
   Papa.parse(url, {
     download: true,
     header: true, // Utilisez false si vous n'avez pas de ligne d'en-tête
